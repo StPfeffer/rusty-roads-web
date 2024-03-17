eeaaa34d4b00b8dee0f6b212517569796f29a90b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-17 15:11:25 -0300

    Adicionada Sidebar na página da folha de pagamento

--- src/app/payroll/Payroll.tsx
+++ src/app/payroll/Payroll.tsx
@@ -1,7 +1,15 @@
+import Sidebar from "@/components/sidebar/Sidebar";
+import { Box } from "@mui/material";
 import React from "react";
 
 const Payroll = () => {
-    return <h1>Folha de Pagamento</h1>
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Sidebar />
+
+      <h1>Folha de Pagamento</h1>
+    </Box>
+  )
 }
 
 export default Payroll;
