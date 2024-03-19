29981e90fb953fabf7e8df23a8b105b17e35c73d
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-19 20:07:54 -0300

    Alteração na nomenclatura de arquivos

--- src/types/CompanyType.ts
+++
@@ -1,9 +0,0 @@
-interface CompanyType {
-    cnpj: string;
-    razaoSocial: string;
-    nomeFantasia: string;
-    endereco: string;
-    regimeTributario: string;
-    inscricaoEstadual: string;
-    inscricaoMunicipal: string;
-}
