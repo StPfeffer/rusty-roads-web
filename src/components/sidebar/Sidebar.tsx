fcf7cd81201a8f742897e91bb03fe9ac6f65671d
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-19 20:10:38 -0300

    Ajuste no design da Sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,151 +1,45 @@
-"use client";
+import React from "react"
 
-import { CSSObject } from "@mui/system";
-import React from "react";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import PaymentsIcon from '@mui/icons-material/Payments';
-import Person2Icon from "@mui/icons-material/Person2";
-import HomeIcon from "@mui/icons-material/Home";
-import BusinessIcon from '@mui/icons-material/Business';
-import NextLink from "next/link";
+import styles from "./sidebar.module.css";
+import { menuItems } from "@/config/routes/menuItems";
+import MenuLink from "./menuLink/menuLink";
+import LogoutIcon from '@mui/icons-material/Logout';
+import Image from "next/image";
 
-import {
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Theme,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const menuRouteList = ["", "company", "contribuitor", "payroll"];
-const menuListTranslations = ["Início", "Empresa", "Colaborador", "Folha de Pagamento"];
-const menuListIcons = [
-  <HomeIcon key="Home" />,
-  <BusinessIcon key="" />,
-  <Person2Icon key="" />,
-  <PaymentsIcon key="" />,
-];
-
-const SideMenu = () => {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  const mobileCheck = useMediaQuery("(min-width: 600px)");
-
-  const handleDrawerToggle = () => {
-    setOpen(!open);
-  };
-
-  const handleListItemButtonClick = (text: string) => {
-    text === "Sign Out" ? console.log("Saindo") : null;
-    setOpen(false);
-  };
-
+const Sidebar = async () => {
   return (
-    <Drawer
-      variant="permanent"
-      anchor="left"
-      open={open}
-      className="drawerHeader"
-      sx={{
-        width: drawerWidth,
-        [`& .MuiDrawer-paper`]: {
-          left: 0,
-          top: mobileCheck ? 0 : 0,
-          flexShrink: 0,
-          whiteSpace: "nowrap",
-          boxSizing: "border-box",
-          ...(open && {
-            ...openedMixin(theme),
-            "& .MuiDrawer-paper": openedMixin(theme),
-          }),
-          ...(!open && {
-            ...closedMixin(theme),
-            "& .MuiDrawer-paper": closedMixin(theme),
-          }),
-        },
-      }}
-    >
-      <div className="drawerHeader">
-        <IconButton onClick={handleDrawerToggle}>
-          {theme.direction === "rtl" ? (
-            <ChevronRightIcon />
-          ) : (
-            <ChevronLeftIcon />
-          )}
-        </IconButton>
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src="/noavatar.png"
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>John Doe</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
       </div>
-      <Divider />
-      <Divider />
-      <List>
-        {menuListTranslations.map((text, index) => (
-          <ListItem key={text} disablePadding sx={{ display: "block" }}>
-            <NextLink
-              className="link"
-              href={`/${menuRouteList[index]}`}
-            >
-              <ListItemButton
-                onClick={() => handleListItemButtonClick(text)}
-                title={text}
-                aria-label={text}
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? "initial" : "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  {menuListIcons[index]}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={{
-                    color: theme.palette.text.primary,
-                    opacity: open ? 1 : 0,
-                  }}
-                />{" "}
-              </ListItemButton>
-            </NextLink>
-          </ListItem>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
         ))}
-      </List>
-    </Drawer>
+      </ul>
+      {/* <form>
+        <button className={styles.logout}>
+          <LogoutIcon />
+          Sair
+        </button>
+      </form> */}
+    </div>
   );
 };
 
-export default SideMenu;
+export default Sidebar;
