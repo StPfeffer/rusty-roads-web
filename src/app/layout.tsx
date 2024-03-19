cd4e21717c825b8db7cf91518a417411365b75ee
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-19 20:07:09 -0300

    Ajuste no layout da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
-import type { Metadata } from "next";
+import React from "react";
+
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
 import "../styles/globals.css";
+import styles from "../components/dashboard/dashboard.module.css";
+import Sidebar from "@/components/sidebar/sidebar";
+import Navbar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +15,22 @@ export const metadata: Metadata = {
   description: "Folha de Pagamento"
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className={styles.container}>
+          <div className={styles.menu}>
+            <Sidebar />
+          </div>
+
+          <div className={styles.content}>
+            <Navbar />
+              {children}
+            {/* <Footer /> */}
+          </div>
+        </div>
+      </body>
     </html>
-  );
+  )
 }
