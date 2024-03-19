f9f30a35e6fdb95a51ca7a1988cd2772581f2e29
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-19 20:08:31 -0300

    Componente para a tela de empresa

---
+++ src/app/company/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { CompanyService } from "@/services/CompanyService";
+
+const Company = () => {
+  const [company, setCompany] = useState<Company>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const companyService = new CompanyService();
+
+      try {
+        const response = await companyService.get();
+
+        setCompany(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Empresa</h1>
+      {company && (
+        <div>
+          <h2>{company.nomeFantasia}</h2>
+          <p>{company.razaoSocial}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Company;
