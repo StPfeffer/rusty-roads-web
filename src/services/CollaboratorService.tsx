d6bd1b86e130ee8d1758007f2b98150f5ead920e
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-26 21:08:33 -0300

    Alterado CollaboratorService de jsx para tsx

--- src/services/CollaboratorService.tsx
+++ src/services/CollaboratorService.tsx
@@ -1,7 +1,8 @@
+import { BASE_URL } from '@/config/url';
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: BASE_URL
 })
 
 export class CollaboratorService {
