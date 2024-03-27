9095632fef8fb5828b10752912a88f308ada4cde
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-26 21:47:28 -0300

    Listar os colaboradores no componente Collaborator

--- src/app/collaborators/page.tsx
+++ src/app/collaborators/page.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+"use client";
 
-const Collaborators = () => {
+import React, { useState, useEffect } from "react";
+import { CollaboratorService } from "@/services/CollaboratorService";
+
+const Collaborator = () => {
+  const [collaborators, setCollaborators] = useState<Colaborador[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const collaboratorService = new CollaboratorService();
+
+      try {
+        const response = await collaboratorService.get();
+
+        setCollaborators(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <h1>Colaboradores</h1>
-  )
+    <div>
+      <h1>Colaboradores</h1>
+      {collaborators.map(collaborator => (
+        <div>
+          <h2>{collaborator.nome}</h2>
+          <p>{collaborator.cpf}</p>
+          <p>{collaborator.rg}</p>
+          <p>{collaborator.cnh}</p>
+          <p>{collaborator.nome}</p>
+          <p>{collaborator.admissao.toString()}</p>
+          <p>{collaborator.genero}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Collaborators;
+export default Collaborator;
