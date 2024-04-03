5358ca2e6c918b6a7efc96d25847773e570dd89a
MateusFindev <finprogramador@gmail.com>
2024-04-02 22:44:48 -0300

    Atualização pagina dos Beneficios

--- src/app/benefits/page.tsx
+++ src/app/benefits/page.tsx
@@ -1,38 +1,65 @@
-import React from "react";
+"use client";
+import { BenefitService } from "@/services/BenefitService";
+import React, { useEffect, useState } from "react";
 
 
-const Benefits = () => {
+const Benefit = () => {
+  const [benefits, setBenefits] = useState<Beneficio[]>([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const benefitService = new BenefitService();
+
+      try {
+        const response = await benefitService.get();
+        setBenefits(response.data);
+
+      } catch (error) {
+        console.log (error);
+      }
+    }
+  })
+
   return (
     <div className="mt-12 w-full ">
       <table className="w-full ring-1  ring-slate-600 rounded-lg">
-          <tr className="">
+        <thead>
+        <tr className="">
             <th className="py-1 overflow-hidden bg-slate-600 rounded-tl-lg w-32">Código</th>
             <th className="py-1 overflow-hidden bg-slate-600 min-w-32">Nome</th>
             <th className="py-1 overflow-hidden bg-slate-600 min-w-32">Descrição</th>
+            <th className="py-1 overflow-hidden bg-slate-600 w-32">Valor Padrão</th>
             <th className="py-1 overflow-hidden bg-slate-600 w-32">Ativo</th>
             <th className="py-1 overflow-hidden bg-slate-600 rounded-tr-lg w-24">Ações</th>
           </tr>
-        <tr className="">
-          <td className="p-2">teste</td>
-          <td className="p-2">teste</td>
-          <td className="p-2">teste</td>
-          <td className="p-2">teteetete</td>
-          <td className="p-2 flex justify-end">
-            <button className="flex items-center bg-slate-800 hover:bg-slate-700 text-white rounded p-1 px-2">🖊</button>
-          </td>
-        </tr>
-        <tr className="">
-          <td className="p-2">teste</td>
-          <td className="p-2">teste</td>
-          <td className="p-2">teste</td>
-          <td className="p-2">teteetete</td>
-          <td className="p-2 flex justify-end">
-            <button className="flex items-center bg-slate-800 hover:bg-slate-700 text-white rounded p-1 px-2">🖊</button>
-          </td>
-        </tr>
+        </thead>
+        <tbody>
+          <tr className="">
+              <td className="p-2">t</td>
+              <td className="p-2">t</td>
+              <td className="p-2">t</td>
+              <td className="p-2">t</td>
+              <td className="p-2">t</td>
+              <td className="p-2 flex justify-end">
+                <button className="flex items-center bg-slate-800 hover:bg-slate-700 text-white rounded p-1 px-2">🖊</button>
+              </td>
+            </tr>
+          {benefits.map(benefit => (
+            <tr className="">
+              <td className="p-2">{benefit.codigo}</td>
+              <td className="p-2">{benefit.nome}</td>
+              <td className="p-2">{benefit.descricao}</td>
+              <td className="p-2">{benefit.valorPadrão}</td>
+              <td className="p-2">{benefit.ativo.toString()}</td>
+              <td className="p-2 flex justify-end">
+                <button className="flex items-center bg-slate-800 hover:bg-slate-700 text-white rounded p-1 px-2">🖊</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   )
 }
 
-export default Benefits;
+export default Benefit;
