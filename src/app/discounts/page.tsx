11381fcca38642355f4d6fc90e0d605ddb717201
MateusFindev <finprogramador@gmail.com>
2024-04-04 19:32:49 -0300

    Atualização da página de Discount

--- src/app/discounts/page.tsx
+++ src/app/discounts/page.tsx
@@ -1,8 +1,65 @@
-import React from "react";
+import { fetchCollaborators } from "@/actions/colaboradores/fetchCollaborators";
+import styles from "../../components/collaborator/collaborators.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { formatarData } from "@/utils/formatarData";
 
-const Discounts = () => {
+import React from "react";
+import { fetchDiscounts } from "@/actions/descontos/fetchDiscounts";
+
+const DiscountsPage = async () => {
+  const discounts = await fetchDiscounts();
+
   return (
-    <h1>Descontos</h1>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <button className={styles.addButton}>Adicionar</button>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Nome</td>
+            <td>Email</td>
+            <td>Admissão</td>
+            <td>Cargo</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {collaborators.map((collaborator) => (
+            <tr key={collaborator.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={"/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {collaborator.nome}
+                </div>
+              </td>
+              <td>{collaborator.email}</td>
+              <td>{formatarData(collaborator.admissao?.toString())}</td>
+              {/* <td>{collaborator.isAdmin ? "Admin" : "Client"}</td> */}
+              {/* <td>{collaborator.isActive ? "active" : "passive"}</td> */}
+              <td>{/*collaborator.cargo.descricao  */} teste</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/collaborators/${collaborator.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      Detalhes
+                    </button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
