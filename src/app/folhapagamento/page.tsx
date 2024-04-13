dd83dd38771f109db2dbe6e7895bb8e8ef8bb696
MateusFindev <finprogramador@gmail.com>
2024-04-13 00:16:04 -0300

    atualização folha pagamento

--- src/app/folhapagamento/page.tsx
+++ src/app/folhapagamento/page.tsx
@@ -4,7 +4,7 @@ const Payroll = () => {
   // const collaborators = await fetchCollaborators();
 
   return (
-    <div className="w-full flex-col gap-2 mt-8 space-y-6">
+    <div className="w-full flex-col gap-2 mt-8 space-y-4">
       <div className="flex justify-end space-x-4">
         <div id="colaborador" className="w-1/4 flex justify-between bg-padrao2 p-4 rounded-lg content-center ">
           <label className=" p-1 font-semibold text-lg ">Colaborador:</label>
@@ -12,12 +12,13 @@ const Payroll = () => {
               <option value="maca">Maçã</option>
               <option value="banana">Banana</option>
               <option value="laranja">Laranja</option>
-              <option value="uva">Uva dsdadsad</option>
+              <option value="uva">Uva dsdads</option>
           </select>
         </div>
         <button className="bg-blue-500 hover:bg-blue-700 text-blue-50 font-bold py-2 px-4 rounded-lg text-lg">Gerar Folha</button>
       </div>
-      <div className="flex space-x-4 ">
+
+      <div className="flex space-x-4 mt-10">
         <div id="informaçõesColaborador" className="flex-none bg-padrao3 p-4 pr-8 rounded-lg">
           
           <h3 className="text-3xl font-bold mb-2 text-blue-400">Mateus Pfeffer</h3>
@@ -30,12 +31,12 @@ const Payroll = () => {
 
         <div className="flex-1 bg-padrao3 p-4 rounded-lg">
           <h3 className="text-xl font-semibold mb-4 ">Salário Base</h3>
-          <p className="text-3xl font-bold text-green-600">R$ 5.000,00</p>
+          <p className="text-4xl font-bold text-green-600">R$ 5.000,00</p>
         </div>
 
         <div className="flex-1 bg-padrao3 p-4 rounded-lg">
           <h3 className="text-xl font-semibold mb-4">Salário Líquido</h3>
-          <p className="text-3xl font-bold text-green-600">R$ 5.000,00</p>
+          <p className="text-4xl font-bold text-green-600">R$ 5.000,00</p>
         </div>
       </div>
 
@@ -47,17 +48,63 @@ const Payroll = () => {
           <h3 className="text-2xl font-bold mt-2 mb-6 w-96">Benefícios</h3>
 
           <div className="flex justify-between px-4 items-center mb-4">
-            <div className="flex-col justify-center">
+            <div className="flex-col justify-center text-gray-200">
               <p className="text-xl">Vale Alimentação</p>
               <p className="text-sm font-thin">Descrição deste benefício, onde tal</p>
             </div>
-            <p>R$ 400,00</p>
+            <p className="text-xl text-green-700">R$ 400,00</p>
+          </div>
+          <div className="flex justify-between px-4 items-center mb-4">
+            <div className="flex-col justify-center text-gray-200">
+              <p className="text-xl">Vale Alimentação</p>
+              <p className="text-sm font-thin">Descrição deste benefício, onde tal</p>
+            </div>
+            <p className="text-xl text-green-700">R$ 400,00</p>
+          </div>
+          <div className="flex justify-between px-4 items-center mb-4">
+            <div className="flex-col justify-center text-gray-200">
+              <p className="text-xl">Vale Alimentação</p>
+              <p className="text-sm font-thin">Descrição deste benefício, onde tal</p>
+            </div>
+            <p className="text-xl text-green-700">R$ 400,00</p>
+          </div>
+
+          <div className="flex justify-between px-4 items-center mt-10 mb-4">
+            <p className="text-2xl font-medium">Total</p>
+            <p className="text-2xl font-medium text-green-600">R$ 400,00</p>
           </div>
         </div>
 
 
         <div id="descontos" className="flex-1 bg-padrao2 p-4 rounded-lg">
           <h3 className="text-2xl font-bold mt-2 mb-6 w-96">Descontos</h3>
+
+          <div className="flex justify-between px-4 items-center mb-4">
+            <div className="flex-col justify-center text-gray-200">
+              <p className="text-xl">Vale Alimentação</p>
+              <p className="text-sm font-thin">Descrição deste desconto, onde tal</p>
+            </div>
+            <p className="text-xl text-red-700">R$ 400,00</p>
+          </div>
+          <div className="flex justify-between px-4 items-center mb-4">
+            <div className="flex-col justify-center text-gray-200">
+              <p className="text-xl">Vale Alimentação</p>
+              <p className="text-sm font-thin">Descrição deste desconto, onde tal</p>
+            </div>
+            <p className="text-xl text-red-700">R$ 400,00</p>
+          </div>
+          <div className="flex justify-between px-4 items-center mb-4">
+            <div className="flex-col justify-center text-gray-200">
+              <p className="text-xl">Vale Alimentação</p>
+              <p className="text-sm font-thin">Descrição deste desconto, onde tal</p>
+            </div>
+            <p className="text-xl text-red-700">R$ 400,00</p>
+          </div>
+
+          <div className="flex justify-between px-4 items-center mt-10 mb-4">
+            <p className="text-2xl font-medium">Total</p>
+            <p className="text-2xl font-medium text-red-600">R$ 400,00</p>
+          </div>
         </div>
       </div>
 
