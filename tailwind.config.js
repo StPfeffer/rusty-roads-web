42c84cb4563a6d53d29ce3c65d9cd63d5b59897d
MateusFindev <finprogramador@gmail.com>
2024-04-13 00:03:06 -0300

    config do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const { green } = require('@mui/material/colors')
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,7 +15,17 @@ module.exports = {
     colors: {
       'padrao2': '#182237',
       'padrao3': '#212F4B',
-      'padrao4': '#27385B'
+      'padrao4': '#27385B',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: colors.blue,
+      green: colors.green
     },
     extend: {},
   },
