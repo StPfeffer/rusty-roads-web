1fed9ac617901886f1ebd81d10160e1d247e7ed3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-13 00:25:49 -0300

    Code smell

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,14 +3,9 @@ import React from "react";
 import Card from "@/components/dashboard/card/card";
 import Transactions from "@/components/dashboard/transactions/transactions";
 import { cards } from "../../db/dummy/cards/data";
-import { fetchCollaborators } from "@/actions/colaboradores/fetchCollaborators";
-import { fetchPayrolls } from "@/actions/folha-pagamento/fetchPayrolls";
 import Rightbar from "@/components/dashboard/rightbar/rightbar";
 
 const Dashboard = async () => {
-  const collaborators = await fetchCollaborators();
-  const payrolls = await fetchPayrolls();
-
   return (
     <div className="flex gap-5 mt-5">
       <div className="flex flex-[3] flex-col gap-5 mt-5">
