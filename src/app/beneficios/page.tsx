f98381799e69c1752de7f1c3b884f2ead2eb9961
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-16 22:57:16 -0300

    Ajuste na sidebar para desativar links com base no arquivo de configuração

--- src/app/beneficios/page.tsx
+++ src/app/beneficios/page.tsx
@@ -10,78 +10,42 @@ const BenefitsPage = async () => {
   return (
     <div className="w-full ">
       <div className="bg-[color:var(--bgSoft)] p-5 mt-5 rounded-lg">
-      <div className="flex justify-between items-center">
-        <Link href="/beneficios/adicionar">
-          <button className="p-2.5 bg-[#5D57C9] border-none pointer rounded text-[color:var(--text)]">Criar Benefício</button>
-        </Link>
-      </div>
-      <table className="w-full">
-        <thead>
-          <tr className="">
-            <td className="pt-2.5 font-bold p-2">Nome</td>
-            <td className="pt-2.5 font-bold p-2">Descrição</td>
-            <td className="pt-2.5 font-bold p-2">Valor Padrão</td>
-            <td className="pt-2.5 font-bold p-2">Ativo</td>
-            <td className="pt-2.5 font-bold p-2 ">Status</td>
-          </tr>
-        </thead>
-        <tbody>
-          {benefits.map((benefit) => (
-            <tr key={benefit.id}>
-              <td className="p-2">{benefit.nome}</td>
-              <td className="p-2">{benefit.descricao}</td>
-              <td className="p-2">{benefit.valorPadrao}</td>
-              <td className="p-2">{benefit.ativo ? "Sim" : "Não"}</td>
-              <td className="p-2">
-                <div className="flex gap-2.5">
-                  <Link href={`/beneficios/${benefit.id}`}>
-                    <button className={`${styles.button} ${styles.view}`}>
-                      Detalhes
-                    </button>
-                  </Link>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-      {/* <table className="w-full ring-1  ring-slate-600 rounded-lg">
-        <thead>
-        <tr className="">
-            <th className="py-1 overflow-hidden bg-slate-600 rounded-tl-lg w-32">Código</th>
-            <th className="py-1 overflow-hidden bg-slate-600 min-w-32">Nome</th>
-            <th className="py-1 overflow-hidden bg-slate-600 min-w-32">Descrição</th>
-            <th className="py-1 overflow-hidden bg-slate-600 w-32">Valor Padrão</th>
-            <th className="py-1 overflow-hidden bg-slate-600 w-32">Ativo</th>
-            <th className="py-1 overflow-hidden bg-slate-600 rounded-tr-lg w-24">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="">
-              <td className="p-2">t</td>
-              <td className="p-2">t</td>
-              <td className="p-2">t</td>
-              <td className="p-2">t</td>
-              <td className="p-2">t</td>
-              <td className="p-2 flex justify-end">
-                <button className="flex items-center bg-slate-800 hover:bg-slate-700 text-white rounded p-1 px-2">🖊</button>
-              </td>
-            </tr>
-          {benefits.map(benefit => (
+        <div className="flex justify-between items-center">
+          <Link href="/beneficios/adicionar">
+            <button className="p-2.5 bg-[#5D57C9] border-none pointer rounded text-[color:var(--text)]" disabled>Criar Benefício</button>
+          </Link>
+        </div>
+        <table className="w-full">
+          <thead>
             <tr className="">
-              <td className="p-2">{benefit.codigo}</td>
-              <td className="p-2">{benefit.nome}</td>
-              <td className="p-2">{benefit.descricao}</td>
-              <td className="p-2">{benefit.valorPadrão}</td>
-              <td className="p-2">{benefit.ativo.toString()}</td>
-              <td className="p-2 flex justify-end">
-                <button className="flex items-center bg-slate-800 hover:bg-slate-700 text-white rounded p-1 px-2">🖊</button>
-              </td>
+              <td className="pt-2.5 font-bold p-2">Nome</td>
+              <td className="pt-2.5 font-bold p-2">Descrição</td>
+              <td className="pt-2.5 font-bold p-2">Valor Padrão</td>
+              <td className="pt-2.5 font-bold p-2">Ativo</td>
+              <td className="pt-2.5 font-bold p-2 ">Status</td>
             </tr>
-          ))}
-        </tbody>
-      </table> */}
+          </thead>
+          <tbody>
+            {benefits.map((benefit) => (
+              <tr key={benefit.id}>
+                <td className="p-2">{benefit.nome}</td>
+                <td className="p-2">{benefit.descricao}</td>
+                <td className="p-2">{benefit.valorPadrao}</td>
+                <td className="p-2">{benefit.ativo ? "Sim" : "Não"}</td>
+                <td className="p-2">
+                  <div className="flex gap-2.5">
+                    <Link href={`/beneficios/${benefit.id}`}>
+                      <button className={`${styles.button} ${styles.view}`} disabled>
+                        Detalhes
+                      </button>
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
