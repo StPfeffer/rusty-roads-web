ab129c823f9d2f9990ff24f1ce74e164b0e3de17
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-06-16 18:13:08 -0300

    Created custom interface to create a vehicle

--- src/types/vehicle.ts
+++ src/types/vehicle.ts
@@ -22,3 +22,21 @@ interface VehicleDocument {
   updatedAt: string,
   vehicleId: string,
 }
+
+interface CreateVehicleData {
+  vehicle: {
+    name: string,
+    initialMileage: string | number,
+  };
+  document: {
+    chassisNumber: string,
+    exerciseYear: string | number,
+    modelYear: string | number,
+    manufactureYear: string | number,
+    registrationNumber: string,
+    color: string,
+    make: string,
+    model: string,
+    plate: string,
+  };
+}
