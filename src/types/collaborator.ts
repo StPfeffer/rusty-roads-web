4a6a6683b6b774399b59f3f4104a5671519c58ee
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-06-18 20:41:48 -0300

    Updated types

--- src/types/collaborator.ts
+++ src/types/collaborator.ts
@@ -4,11 +4,31 @@ interface Collaborator {
   cpf: string,
   rg: string,
   email: string,
+  driver?: Driver,
   createdAt: string,
   updatedAt: string,
 }
 
 interface Driver {
   id: string,
-  //TODO
+  cnh_number: string,
+  cnh_expiration_date: string,
+  id_cnh_type: string,
+  collaboratorId: string,
+  createdAt: string,
+  updatedAt: string,
+}
+
+interface CreateCollaboratorData {
+  collaborator: {
+    name: string,
+    cpf: string,
+    rg: string,
+    email: string,
+  };
+  driver: {
+    cnh_number: string,
+    cnh_expiration_date: string,
+    id_cnh_type: string,
+  };
 }
